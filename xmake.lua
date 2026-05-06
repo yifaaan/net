a7f0f55ab88a4934e22220aba46ca64a51cfaa0c
yifaaan <0219lyf@gmail.com>
2026-05-06 13:09:49 +0800

    build(xmake): add xmake build config

---
+++ xmake.lua
@@ -0,0 +1,52 @@
+set_project("net")
+set_version("0.1.0")
+
+set_languages("c++20")
+add_rules("mode.debug", "mode.release")
+
+local net_sources = {
+  "src/inet_address.cc",
+  "src/epoll.cc",
+  "src/channel.cc",
+  "src/event_loop.cc",
+  "src/tcp_server.cc",
+  "src/acceptor.cc",
+  "src/connection.cc",
+  "src/buffer.cc",
+  "src/echo_server.cc",
+  "src/thread_pool.cc",
+}
+
+target("net_main")
+  set_kind("binary")
+  add_includedirs("src")
+  add_files("src/tcpepoll.cc")
+  add_files(net_sources)
+
+target("net_client")
+  set_kind("binary")
+  add_files("src/client.cc")
+
+target("echo_client")
+  set_kind("binary")
+  add_files("examples/echo/client.cpp")
+
+target("echo_server")
+  set_kind("binary")
+  add_files("examples/echo/server.cpp")
+
+target("file_trans_client")
+  set_kind("binary")
+  add_files("examples/file_transfor/client.cpp")
+
+target("file_trans_server")
+  set_kind("binary")
+  add_files("examples/file_transfor/server.cpp")
+
+target("client_multi")
+  set_kind("binary")
+  add_files("examples/multi_process/client.cpp")
+
+target("server_multi")
+  set_kind("binary")
+  add_files("examples/multi_process/server.cpp")
